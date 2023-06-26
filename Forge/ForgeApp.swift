5b5baa90716125a244e0bb6310e475a111f4e374
Ryan Divigalpitiya <ryan.div@icloud.com>
2023-06-25 21:26:06 -0400

    Initial Commit

---
+++ Forge/ForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForgeApp.swift
+//  Forge
+//
+//  Created by Ryan Div on 2023-06-25.
+//
+
+import SwiftUI
+
+@main
+struct ForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
